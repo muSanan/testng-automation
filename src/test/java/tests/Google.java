62b0aa4b2bb9ac1e77c3023e120011c91dca1438
Sanan <musan.bkt@gmail.com>
2023-06-08 21:31:25 -0500

    added quit and deleted simple test

--- src/test/java/tests/Google.java
+++ src/test/java/tests/Google.java
@@ -20,16 +20,7 @@ public class Google {
         Driver.getDriver().findElement(By.name("q")).sendKeys(testData + Keys.ENTER);
         String tittle = Driver.getDriver().getTitle();
         Assert.assertTrue("Tittle failed",tittle.contains(testData));
-
-    }
-
-    @Test
-    public void simple(){
-        Driver.getDriver().get(Config.getValue("URL"));
-        Driver.getDriver().findElement(By.name("username")).sendKeys(Config.getValue("username1"));
-        Driver.getDriver().findElement(By.name("password")).sendKeys(Config.getValue("password1")+Keys.ENTER);
         Driver.quit();
+
     }
-
-
 }
