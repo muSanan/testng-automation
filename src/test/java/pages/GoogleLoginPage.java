e028090e388f3b4d77e7d7888b3e76000a529943
Sanan <musan.bkt@gmail.com>
2023-06-09 20:43:53 -0500

    GoogleLoginPage is created

---
+++ src/test/java/pages/GoogleLoginPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class GoogleLoginPage {
+    public GoogleLoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
